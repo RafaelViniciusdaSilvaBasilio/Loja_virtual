e606d73e1ad0d5cf22e80f596f2bad2899d3b5d9
Rafael Vinicius da Silva Basilio <rafaelbasilio2004@gmail.com>
2025-11-03 22:34:20 -0300

    Commit tudo

---
+++ src/main/java/com/LojaVirtual/demo/model/PessoaFisica.java
@@ -0,0 +1,39 @@
+package com.LojaVirtual.demo.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "pessoa_fisica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaFisica extends Pessoa {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataNascimento;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+}
